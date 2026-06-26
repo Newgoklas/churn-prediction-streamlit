de22a8198a228b87e7a738f46f75227c75b624b6
Newgoklas <147721502+Newgoklas@users.noreply.github.com>
2026-06-26 17:29:12 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -71,10 +71,9 @@ MODEL_DIR = Path("models")
 @st.cache_resource
 def load_artifacts():
     """Load semua file model menggunakan joblib (lebih kompatibel)"""
-    artifacts = {}
+    artifacts = {}s
     files_needed = {
         'model'      : MODEL_DIR / 'best_model.pkl',
-        'scaler_top' : MODEL_DIR / 'scaler_top.pkl',
         'scaler'     : MODEL_DIR / 'scaler.pkl',
         'label_enc'  : MODEL_DIR / 'label_encoders.pkl',
         'top_feat'   : MODEL_DIR / 'top_features.pkl',
